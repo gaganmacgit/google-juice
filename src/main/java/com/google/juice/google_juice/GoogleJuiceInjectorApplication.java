e1d89bfc33ffe111ebdbf08f6415936c26cba02b
Gagan Gupta <gagan@Gagans-MacBook-Pro.local>
2017-12-02 23:24:04 +0530

    Extra commit to be rolled back later

--- src/main/java/com/google/juice/google_juice/GoogleJuiceInjectorApplication.java
+++ src/main/java/com/google/juice/google_juice/GoogleJuiceInjectorApplication.java
@@ -41,7 +41,8 @@ public class GoogleJuiceInjectorApplication {
 		House house4 = injector.getInstance(House.class);
 		House house5 = injector.getInstance(House.class);
 		House house6 = injector.getInstance(House.class);
-		House house7 = injector.getInstance(House.class);		
+		House house7 = injector.getInstance(House.class);
+		House house8 = injector.getInstance(House.class);		
 		// injector.getInstance(House.class);
 		house1.getWindow().constructorMe();
 	}
